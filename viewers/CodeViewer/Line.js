bdd99bc0983299798d62a070cdb4b317f9236424
Terry Weiss <ttweiss@syr.edu>
2020-03-07 13:36:28 -0500

    Completed Codeviewer display and hex conversion of big nums

--- viewers/CodeViewer/Line.js
+++ viewers/CodeViewer/Line.js
@@ -6,19 +6,37 @@ import { LabelLine, InstrLine } from './LineTypes';
 
 export default function Line(props) {
   const { lineAddr, label, mnemonic, args, funcAddr, blockAddr, isLastLine } = props;
-//  const { gutterWidth, setGutterWidth } = useContext(Context);
+  const { gutterWidth, setGutterWidth } = useContext(Context);
 
-  if (label)
-    return (<LabelLine key={`line${lineAddr}`} label={`${label}`} />);
-  return (<InstrLine key={`line${lineAddr}`} addr={lineAddr} mnemonic={mnemonic} args={args} />);
+  if (label) {
+    return (
+      <div key={`line${lineAddr}div`}>
+        <Gutter
+            lineAddr={ lineAddr }
+            lastLine={ isLastLine }
+            width={ gutterWidth }
+            onSet={ setGutterWidth } />
+          <LabelLine key={`line${lineAddr}`} style={{display: 'inline-block'}} label={`${label}`} />
+      </div>
+    );
+  }
+  return (
+    <div key={`line${lineAddr}div`}>
+      <Gutter
+        lineAddr={ lineAddr }
+        lastLine={ isLastLine }
+        width={ gutterWidth }
+        onSet={ setGutterWidth } />
+      <InstrLine style={{display: 'inline-block'}} key={`line${lineAddr}`} addr={lineAddr} mnemonic={mnemonic} args={args} />
+    </div>
+  );
 }
 
-/*
 function Gutter(props) {
   const { lineAddr, lastLine, width, theme, onSet } = props;
 
   function set(ref) {
-    if (lastLine) {
+    if (ref && lastLine) {
       const bounds = ref.getBoundingClientRect();
       const currWidth = bounds.width;
       if (width !== currWidth) onSet(currWidth);
@@ -44,4 +62,3 @@ function Gutter(props) {
     </div>);
 }
 Gutter = withTheme(Gutter);
-*/
--- viewers/CodeViewer/LineTypes.js
+++ viewers/CodeViewer/LineTypes.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core';
 
 const INSTR_MARGIN = '0px 0px 0px 40px';
-const INSTR_WIDTH = '150px';
+const INSTR_WIDTH = '80px';
+
+const AUTO_HEX_DIGITS = 6;
 
 const labelColor = '#0016b5'        // Dark blue
 const textColor = '#00a5e7'         // Dark cyan
@@ -11,7 +13,7 @@ const useStyles = makeStyles({
   style: props => ({
     fontFamily: '"Roboto Mono", "Courier New", Courier, monospace',
     fontWeight: 'bold',
-    display: 'block-inline',
+    display: 'inline-block',
     color: (props.color || textColor),
     padding: 0,
     margin: 0,
@@ -20,7 +22,7 @@ const useStyles = makeStyles({
   instr: props => ({
     fontFamily: '"Roboto Mono", "Courier New", Courier, monospace',
     fontWeight: 'bold',
-    display: 'block-inline',
+    display: 'inline-block',
     color: (props.color || textColor),
     padding: 0,
     whitespace: 'pre',
@@ -47,7 +49,7 @@ export function InstrLine(props) {
     spans.slice(-1); // remove separator
   }
   return (
-    <div key={`line${addr}-root`} id={`line${addr}-root`}>
+    <div style={{display:'inline-block'}} key={`line${addr}-root`} id={`line${addr}-root`}>
       {spans}
     </div>
   );
@@ -78,10 +80,53 @@ function TextSpan(props) {
   return (<span className={style}>{props.code}</span>);
 }
 
-// TODO Numbers longer than AUTO_HEX_DIGITS digits should be automatically converted to hex
 // TODO Numbers should be double-clickable to toggle between hex and decimal
 function NumSpan(props) {
   const opts = {color: numColor};
   const style = (props.style || useStyles(opts).style);
-  return (<span className={style}>{props.code}</span>);
+  let nStr = props.code;
+  if (nStr.length > 6) {
+    nStr = numToHex(nStr);
+  }
+  return (<span className={style}>{nStr}</span>);
+}
+
+const numToHex = nStr => {
+  let positive = true;
+  let bigNum = BigInt(nStr);
+
+  if (bigNum < 0) {
+    positive = false;
+    bigNum = twosCompliment(bigNum);
+  }
+
+  // Pad an uneven number of bits with 0 for parsability
+  let hex = bigNum.toString(16);
+  if (hex.length % 2) {
+    hex = '0' + hex;
+  }
+
+  // Pad an extra byte to avoid ambiguity if positive and first bit is set
+  let hiByte = parseInt(hex.slice(0, 2), 16);
+  let hiBit = (0x80 & hiByte);
+  if (positive && hiBit) {
+    hex = '00' + hex;
+  }
+
+  return '0x' + hex;
+}
+
+// Perform Two's Compliment to get opposite signed hex value
+function twosCompliment(bigNum) {
+  bigNum = -bigNum;
+  let binary = (bigNum).toString(2)
+  let prefix = '';
+  while (binary.length % 8) {
+    binary = '0' + binary;
+  }
+  if ('1' === binary[0] && -1 !== binary.slice(1).indexOf('1')) {
+    prefix = '11111111';
+  }
+  binary = binary.split('').map(i => ('0' === i ? '1' : '0')).join('');
+  return BigInt('0b' + prefix + binary) + BigInt(1);
 }
