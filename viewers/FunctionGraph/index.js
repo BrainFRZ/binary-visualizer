6742bb25fc9e4f3f73fd142f875e4ccc9504aa4f
Terry Weiss <ttweiss@syr.edu>
2020-03-07 22:16:30 -0500

    Updated color palette of Funcs graph to match codeviewer

--- viewers/FunctionGraph/index.js
+++ viewers/FunctionGraph/index.js
@@ -25,27 +25,27 @@ const mainConfig = {
   style: [{
       selector: 'node',
       style: {
-        'background-color': '#6783F3',
+        'background-color': '#0016b5', // dark blue
         'label': 'data(label)',
         'text-valign': 'center',
         'height': '42px',
         'width': calcWidth(),
         'text-wrap': 'wrap',
         'font-family': 'Roboto Mono',
-        'color': '#E93251',
-        'border-width': '2px',
-        'border-color': '#0017a6',
-        'font-weight': 'bold',
+        'color': '#FFFFFF', // white
+        'border-width': '1px',
+        'border-color': '#000000',
+        'font-weight': 'bold'
       }
     }, {
       selector: 'edge',
       style: {
         'label': 'data(label)',
         'curve-style': 'bezier',
-        'line-color': getStyle('line-color', '#0017a6'), // default dark blue
+        'line-color': getStyle('line-color', '#6783F3'), // default dark blue
         'line-style': getStyle('line-style', 'solid'),
         'target-arrow-shape': 'triangle',
-        'target-arrow-color': getStyle('target-arrow-color', '#0017a6')
+        'target-arrow-color': getStyle('target-arrow-color', '#6783F3')
       }
     },
   ],
